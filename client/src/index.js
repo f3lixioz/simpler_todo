475ab1d3a00e5aa6be14398b2b8431c591716594
f3lixioz <f3lixioz@gmail.com>
2021-07-21 03:20:07 -0600

    Heroku dep try 9

--- client/src/index.js
+++ client/src/index.js
@@ -11,10 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res)=>{
-  res.sendFile(path.join(__dirname, "client/build",'index.html'));
-})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
